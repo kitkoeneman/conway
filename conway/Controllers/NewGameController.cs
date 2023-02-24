2266cca33f2ab9ce74d99232eef84c3ef63d1665
Kit Koeneman <59888890+kitkoeneman@users.noreply.github.com>
2023-02-24 18:06:12 +0000

    partial commit

---
+++ conway/Controllers/NewGameController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace conway.Controllers;
+
+public class NewGameController : Controller
+{
+    // 
+    // GET: /NewGame/
+    public string Index()
+    {
+        return "This is my default action...";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
+}
