ef86cf7e9fc31b6684109ab579dcc4ffeba83c71
Kit Koeneman <59888890+kitkoeneman@users.noreply.github.com>
2023-03-02 19:42:50 +0000

    Add constructor and validation requirements to GameBoard model

--- conway/Models/GameBoard.cs
+++ conway/Models/GameBoard.cs
@@ -1,9 +1,22 @@
-﻿namespace conway.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace conway.Models
 {
     public class GameBoard
     {
-        public int Height { get; }
-        public int Width { get; }
-        public int[,,]? Board { get; }
+        [Range(10,100)]
+        public int Height { get; set; }
+        
+        [Range(10,100)]
+        public int Width { get; set; }
+        public int[,,]? Board { get; set; }
+
+        public GameBoard(int height, int width)
+        {
+            Height = height; 
+            Width = width;
+        }
     }
+
+
 }
